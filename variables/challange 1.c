317f8f35645c75d29ff5a4fb8bbdcf57ceb05d7f
brahim <brahim888@gmail.com>
2025-09-02 11:47:20 +0100

    challange 3

--- variables/challange 1.c
+++ variables/challange 1.c
@@ -1,50 +1,35 @@
 #include <stdio.h>
 
-// challange 1
-//int main(){
-//	char first_name[20] = "";
-//	char last_name[20] = "";
-//	int age = 0;
-//	char sex[6] = "";
-//	char email[50] = "";
-//	
-//	
-//	printf("enter your first name: ");
-//	scanf("%s", &first_name);
-//	
-//	printf("enter your last name: ");
-//	scanf("%s", &last_name);
-//	
-//	printf("enter your age: ");
-//	scanf("%d", &age);
-//	
-//	printf("enter your sex: ");
-//	scanf("%s", &sex);
-//	
-//	printf("enter your email: ");
-//	scanf("%s", &email);
-//	
-//	
-//	printf("Hi! your name is %s %s, ", first_name, last_name);
-//	printf("and you are %d years old.\n", age);
-//	printf("your sex is: %s\n", sex);
-//	printf("your email is: %s\n", email);
-//	
-//	
-//  	return 0;
-//}
-
-
-//challange 2
-int main (){
-	float temp_C = 0.0f;
-	float temp_K = 0.0f;
+int main(){
+	char first_name[20] = "";
+	char last_name[20] = "";
+	int age = 0;
+	char sex[6] = "";
+	char email[50] = "";
 	
-	printf("enter the tempereture in Celsius: ");
-	scanf("%f", &temp_C);
 	
-	temp_K = temp_C + 273.15;
+	printf("enter your first name: ");
+	scanf("%s", &first_name);
 	
-	printf("%.2f celsius in kelvin is: %.2f", temp_C, temp_K);
+	printf("enter your last name: ");
+	scanf("%s", &last_name);
 	
+	printf("enter your age: ");
+	scanf("%d", &age);
+	
+	printf("enter your sex: ");
+	scanf("%s", &sex);
+	
+	printf("enter your email: ");
+	scanf("%s", &email);
+	
+	
+	printf("Hi! your name is %s %s, ", first_name, last_name);
+	printf("and you are %d years old.\n", age);
+	printf("your sex is: %s\n", sex);
+	printf("your email is: %s\n", email);
+	
+	
+  	return 0;
 }
+
---
+++ variables/challange 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+	float kilometers = 0.0f;
+	float yards = 0.0f;
+	
+	printf("enter the distance in kilometers: ");
+	scanf("%f", &kilometers);
+	
+	yards = kilometers * 1093.61;
+	
+	printf("%.2f kilometes is: %.2f yards", kilometers, yards);
+}
